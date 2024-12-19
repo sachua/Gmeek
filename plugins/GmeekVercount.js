727dd2e9991875c4278ff3e5994b4169dc485046
sachua <chuasongann@gmail.com>
2024-12-19 15:15:17 +0800

    Update english

--- plugins/GmeekVercount.js
+++ plugins/GmeekVercount.js
@@ -1,10 +1,10 @@
 function createVercount() {
     var postBody = document.getElementById('postBody');
     if (postBody){
-        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">Page visited <span id="busuanzi_value_page_pv"></span> times</div>');
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;"><span id="busuanzi_value_page_pv"></span> views</div>');
     }
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">Site visited <span id="busuanzi_value_site_pv"></span> times • </span>');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none"><span id="busuanzi_value_site_pv"></span> visits • </span>');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
