f80ceda019a08c9d3ba716d939af681fae3b386b
sachua <chuasongann@gmail.com>
2024-12-19 14:36:13 +0800

    Update vercount

--- plugins/GmeekVercount.js
+++ plugins/GmeekVercount.js
@@ -1,10 +1,10 @@
 function createVercount() {
     var postBody = document.getElementById('postBody');
     if (postBody){
-        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">本文浏览量<span id="busuanzi_value_page_pv"></span>次</div>');
+        postBody.insertAdjacentHTML('afterend','<div id="busuanzi_container_page_pv" style="display:none;float:left;margin-top:8px;font-size:small;">Page visited<span id="busuanzi_value_page_pv"></span>times</div>');
     }
     var runday = document.getElementById('runday');
-    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">总浏览量<span id="busuanzi_value_site_pv"></span>次 • </span>');
+    runday.insertAdjacentHTML('afterend', '<span id="busuanzi_container_site_pv" style="display:none">Site visited<span id="busuanzi_value_site_pv"></span>times • </span>');
 }
 
 document.addEventListener("DOMContentLoaded", function() {
